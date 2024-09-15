65574ff98fa052bed45df81eed5727e54416ab87
Wojciech Jan Bozek <1733-wbozek@users.noreply.gitlab-stud.elka.pw.edu.pl>
2024-09-15 21:02:37 +0200

    Test code

---
+++ src/main.cpp
@@ -0,0 +1,217 @@
+// OpenGLCourseApp.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+//Window dimensions
+constexpr GLint WIDTH = 800, HEIGHT = 600;
+
+GLuint VAO, VBO, shader;
+
+void CreateTriangle()
+{
+    GLfloat vertices[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+// Vertexshader
+static const char* vShader = "\n\
+#version 330\n\
+layout  (location = 0) in vec3 pos;\n\
+\n\
+void  main()\n\
+{\n\
+    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);\n\
+}\n";
+
+static const char* fShader = "\n\
+#version 330\n\
+out vec4 color;\n\
+\n\
+void main()\n\
+{\n\
+    color = vec4(1.0, 0.0, 0.0, 1.0);\n\
+}\n";
+
+
+void addShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
+{
+    GLuint theShader = glCreateShader(shaderType);
+
+    const GLchar *theCode[1];
+    theCode[0] = shaderCode;
+
+    GLint codeLength[1];
+    codeLength[0] = strlen(shaderCode);
+
+    glShaderSource(theShader, 1, theCode, codeLength);
+
+    glCompileShader(theShader);
+    
+    GLint result = 0;
+    GLchar eLog[1024] = { 0 };
+
+    glGetShaderiv(theShader, GL_COMPILE_STATUS, &result);
+    if (!result)
+    {
+        glGetShaderInfoLog(theShader, sizeof(eLog), NULL, eLog);
+        printf("Error Compiling the %d shader!!: %s \n", shaderType, eLog);
+        return;
+    }
+
+    glAttachShader(theProgram, theShader);
+}
+
+void compileShaders() 
+{
+    shader = glCreateProgram();
+    if (!shader)
+    {
+        printf("Error creating shaders");
+        return;
+    }
+    addShader(shader, vShader, GL_VERTEX_SHADER);
+    addShader(shader, fShader, GL_FRAGMENT_SHADER);
+
+    GLint result = 0;
+    GLchar eLog[1024] = { 0 };
+
+    glLinkProgram(shader);
+
+    //linking error
+    glGetProgramiv(shader, GL_LINK_STATUS, &result);
+    if (!result)
+    {
+        glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+        printf("Error Linkig Program!!: %s \n", eLog);
+        return;
+    }
+
+    //linking validation
+    glValidateProgram(shader);
+    glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+    if (!result)
+    {
+        glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+        printf("Error Validating Program!!: %s \n", eLog);
+        return;
+    }
+}
+
+int main()
+{
+    // Initialize GLFW
+    if (!glfwInit())
+    {
+        printf("GLFW initialization failed");
+        glfwTerminate();
+        return 1;
+    }
+
+    // Setup GLFW window properties
+    //Opengl version
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // core profile = no backwards compatibility
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // allow foreward compatybility
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "First window", NULL, NULL);
+
+    if (!mainWindow)
+    {
+        printf("GLFWwindow creation failed!");
+        glfwTerminate();
+        return 1;
+    }
+
+    // Get Buffer size information
+    int  bufferWidth, bufferHeight;
+    glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+    // Set context for GLEW to use
+    glfwMakeContextCurrent(mainWindow);
+
+    //Allow modern extension feature;
+    glewExperimental = GL_TRUE;
+
+    if (glewInit() != GLEW_OK)
+    {
+        printf("Glew initialization failed!");
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+        return 1;
+    }
+
+    //Setup Viewport Size
+
+    glViewport(0, 0, bufferWidth, bufferHeight);
+
+    CreateTriangle();
+
+    compileShaders();
+
+    //Loop until window close
+
+    while (!glfwWindowShouldClose(mainWindow))
+    {
+        // get + handel user input events
+        glfwPollEvents();
+
+        // clear window
+        glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader);
+
+        glBindVertexArray(VAO);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glBindVertexArray(0);
+
+        glUseProgram(0);
+
+        glfwSwapBuffers(mainWindow);
+    }
+
+    glfwDestroyWindow(mainWindow);
+
+
+    return 0;
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć n błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
